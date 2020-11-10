2d85e73ee5ddcf21568e17d6fa69e2cf338fabb1
bfeldman <brian.feldman42@gmail.com>
2020-11-09 23:17:53 -0500

    added new task form, corresponding js selectors

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@ const baseUrl = "http://localhost:3000/api/v1"
 fetch(`${baseUrl}/users/1`)
 .then(response => response.json())
 .then(data => console.log(data))
+
+
+// NEW TASK FORM
+const newTaskForm = document.querySelector("#new-task-form")
+const newTaskTitle = newTaskForm.querySelector("input[name='title']")
+const newTaskDueDate = newTaskForm.querySelector("input[name='due-date']")
+const newTaskPriority = newTaskForm.querySelector("select[name='priority']")
+const newTaskTag = newTaskForm.querySelector("input[name='tag']")
+const newTaskDescription = newTaskForm.querySelector("input[name='description']")
+
+newTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log("Task:", newTaskTitle.value)
+    console.log("Due date:", newTaskDueDate.value)
+    console.log("Priority:", newTaskPriority.value)
+    console.log("Tag:", newTaskTag.value)
+    console.log("Task:", newTaskTitle.value)
+})
