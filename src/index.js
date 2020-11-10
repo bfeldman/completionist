c775507c2b3c56a690791d23173b120c57bcbffb
bfeldman <brian.feldman42@gmail.com>
2020-11-10 13:50:19 -0500

    new task form refactor

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const loginForm = document.querySelector("#login")
 // main interface
 const mainElement = document.querySelector("main")
 const mainContainer = document.querySelector("#main-container")
+const newTaskButton = document.querySelector("#new-task-button")
 
 
 /* FUNCTIONS */ 
@@ -35,60 +36,131 @@ loginForm.addEventListener("submit", (e) => {
 
 
 // new task form render
-const newTaskButton = document.querySelector("#new-task-button")
 newTaskButton.addEventListener("click", () => {
-    if (!newTaskForm) {
+    if (!document.querySelector("#new-task-form")) {
         const taskForm = document.createElement("form")
             taskForm.id = "new-task-form"
-        taskForm.innerHTML = `
-            <button id="close-form" type="button">X</button>
-            <h4>Add Task</h4>
-            <input type="text" name="title" placeholder="task"/><br>
-            <label>Due date:</label><input type="datetime-local" name="due-date"><br>
-            <label>Priority:</label><select name="priority">
-                  <option value="2">High</option>
-                  <option value="1">Normal</option>
-                  <option value="0">Low</option>
-            </select><br>
-            <label>Tag:</label><input type="text" name="tag" placeholder="tag"/><br>
-            <label>Description:</label><input type="text" name="description" placeholder="description"/><br>
+
+        const closeFormButton = document.createElement("button")
+            closeFormButton.id = "close-form"
+            closeFormButton.setAttribute("type", "button")
+            closeFormButton.textContent = "❌ Close Form"
+        
+        const taskFormHeader = document.createElement("h4")
+            taskFormHeader.textContent = "Add Task"
+        
+        const taskTitleInput = document.createElement("input")
+            taskTitleInput.setAttribute("type", "text")
+            taskTitleInput.setAttribute("name", "title")
+            taskTitleInput.setAttribute("placeholder", "task title")
+            taskTitleInput.append(document.createElement("br"))
+        
+        const taskDueDateLabel = document.createElement("label")
+            taskDueDateLabel.textContent = "Due date:"
+        const taskDueDateSelect = document.createElement("input")
+            taskDueDateSelect.setAttribute("type", "datetime-local")
+            taskDueDateSelect.setAttribute("name", "due-date")
+            taskDueDateSelect.append(document.createElement("br"))
+        
+        const taskPriorityLabel = document.createElement("label")
+            taskPriorityLabel.textContent = "Priority:"
+        const taskPrioritySelect = document.createElement("select")
+            taskPrioritySelect.setAttribute("name", "priority")
+            const highPriority = document.createElement("option")
+                highPriority.setAttribute("value", "2")
+                highPriority.textContent = "High"
+            const normalPriority = document.createElement("option")
+                normalPriority.setAttribute("value", "1")
+                normalPriority.textContent = "Normal"
+            const lowPriority = document.createElement("option")
+                lowPriority.setAttribute("value", "0")
+                lowPriority.textContent = "Low"
+            taskPrioritySelect.append(highPriority, normalPriority, lowPriority)
+        
+        const taskTagLabel = document.createElement("label")
+            taskTagLabel.textContent="Tag:"
+        const taskTagInput = document.createElement("input")
+            taskTagInput.setAttribute("type", "text")
+            taskTagInput.setAttribute("name", "tag")
             
-            <input type="submit" value="Add Task" />
-        `
+        const taskDescriptionLabel = document.createElement("label")
+            taskDescriptionLabel.textContent="Description:"
+        const taskDescriptionInput = document.createElement("input")
+            taskDescriptionInput.setAttribute("type", "text")
+            taskDescriptionInput.setAttribute("name", "description")
+            
+        const submitNewTaskButton = document.createElement("input")
+            submitNewTaskButton.setAttribute("type", "submit")
+            submitNewTaskButton.setAttribute("value", "Add New Task")
+
+            
+        taskForm.append(
+            closeFormButton,
+            taskFormHeader,
+            taskTitleInput,
+            document.createElement("br"),
+            taskDueDateLabel,
+            taskDueDateSelect,
+            document.createElement("br"),
+            taskPriorityLabel,
+            taskPrioritySelect,
+            document.createElement("br"),
+            taskTagLabel,
+            taskTagInput,
+            document.createElement("br"),
+            taskDescriptionLabel,
+            taskDescriptionInput,
+            document.createElement("br"),
+            submitNewTaskButton
+        )
+        
         mainContainer.append(taskForm)
+        
+        //new task form submit
+        const newTaskForm = document.querySelector("#new-task-form")
+        newTaskForm.addEventListener("submit", (event) => {
+            event.preventDefault()
+            // get HTML form inputs
+            const newTaskTitle = newTaskForm.querySelector("input[name='title']").value
+            const newTaskDueDate = newTaskForm.querySelector("input[name='due-date']").value
+            const newTaskPriority = newTaskForm.querySelector("select[name='priority']").value
+            const newTaskTag = newTaskForm.querySelector("input[name='tag']").value
+            const newTaskDescription = newTaskForm.querySelector("input[name='description']").value
+        
+            const newTask = {
+                title: newTaskTitle,
+                priority_level: newTaskPriority, 
+                due_date: newTaskDueDate,
+                tag: newTaskTag, 
+                description: newTaskDescription, 
+                completion_status: false,
+                user_id: mainContainer.dataset.id
+            }
+                    
+            fetch(baseUrl+'/tasks', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(newTask)
+            })
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                // newTaskForm.remove()
+                // RENDER NEW TASK SUBMISSION DETAILS
+            })
+        })
+        
+        // close new task form
+        const closeFormBtn = newTaskForm.querySelector("#close-form")
+        closeFormBtn.addEventListener("click", () => {
+            newTaskForm.remove()
+        })
+        
     }
 })
 
-//new task form submit
-const newTaskForm = document.querySelector("#new-task-form")
-newTaskForm.addEventListener("submit", (event) => {
-    // get HTML form elements
-    const newTaskTitle = newTaskForm.querySelector("input[name='title']")
-    const newTaskDueDate = newTaskForm.querySelector("input[name='due-date']")
-    const newTaskPriority = newTaskForm.querySelector("select[name='priority']")
-    const newTaskTag = newTaskForm.querySelector("input[name='tag']")
-    const newTaskDescription = newTaskForm.querySelector("input[name='description']")
-    
-    event.preventDefault()
 
-    const newTask = {
-        title: newTaskTitle.value,
-        priority_level: newTaskPriority.value, 
-        due_date: newTaskDueDate.value,
-        tag: newTaskTag.value, 
-        description: newTaskDescription.value, 
-        completion_status: false,
-        user_id: mainContainer.dataset.id
-    }
-
-    fetch(baseUrl+'/tasks', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(newTask)
-    })
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-})
+// edit function
