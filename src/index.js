5665258862b5263c6985ad8994f2fe36ada94dfd
billyott <willrichott@gmail.com>
2020-11-10 10:52:53 -0500

    test fetch requests

--- src/index.js
+++ src/index.js
@@ -19,5 +19,55 @@ newTaskForm.addEventListener("submit", (event) => {
     console.log("Due date:", newTaskDueDate.value)
     console.log("Priority:", newTaskPriority.value)
     console.log("Tag:", newTaskTag.value)
+    console.log("Description:", newTaskDescription.value)
+
+    const newTask = {
+        title: newTaskTitle.value,
+        priority_level: newTaskPriority.value, 
+        due_date: newTaskDueDate.value,
+        tag: newTaskTag.value, 
+        description: newTaskDescription.value, 
+        completion_status: false,
+        user_id: 12
+    }
+
+    fetch(baseUrl+'/tasks', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(newTask)
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+})
+
+newTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault()
     console.log("Task:", newTaskTitle.value)
+    console.log("Due date:", newTaskDueDate.value)
+    console.log("Priority:", newTaskPriority.value)
+    console.log("Tag:", newTaskTag.value)
+    console.log("Description:", newTaskDescription.value)
+
+    const newTask = {
+        title: newTaskTitle.value,
+        priority_level: newTaskPriority.value, 
+        due_date: newTaskDueDate.value,
+        tag: newTaskTag.value, 
+        description: newTaskDescription.value, 
+        completion_status: true,
+        user_id: 12
+    }
+
+    fetch(baseUrl+'/tasks/53', {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
 })
